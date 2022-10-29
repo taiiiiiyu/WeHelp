a612a6b3c6b38d8b97cdb6db0749808e276c2365
TaiYuWang <cwangty@gmail.com>
2022-10-30 00:31:06 +0800

    update week6

--- week6/app.py
+++ week6/app.py
@@ -1,16 +1,18 @@
 from flask import Flask,request,render_template,redirect,session
-from flask_mysqldb import MySQL
-# import MySQLdb.cursors
+import mysql.connector
 
 
 app=Flask(__name__,static_folder="public",static_url_path="/")
 app.secret_key="any string but secret"
-app.config['MYSOL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='12345678'
-app.config['MYSQL_DB']='website'
-app.config['MYSQL_PORT']='3306'
-mysql=MySQL(app)
+
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="12345678",
+    database="website"
+)
+
 
 #首頁
 @app.route("/")
@@ -23,6 +25,7 @@ def member():
     if "username" in session:
         return render_template("member.html")
     return render_template("index.html")
+
 #失敗頁面網址
 @app.route("/error")
 def error():
@@ -34,31 +37,48 @@ def error():
 def signin():
     usernameData=request.form["usernameData"]
     passwordData=request.form["passwordData"]
-    if usernameData=="test" and passwordData=="test":
-        session["username"]=usernameData
-        return redirect("/member")
+    mycursor=mydb.cursor()
+    mycursor.execute("SELECT * FROM member WHERE username =%s",(usernameData,))
+    userN=mycursor.fetchone()
+    mycursor.execute("SELECT * FROM member WHERE password =%s",(passwordData,))
+    userP=mycursor.fetchone()
     if usernameData=="" or passwordData == "":
         return redirect("/error?message=請輸入帳號、密碼")
+    elif userN!=None and userP!=None:
+        session["username"]=usernameData
+        return redirect("/member")
+    elif userN==None:
+        return redirect("/error?message=沒有此帳號")
     else:
         return redirect("/error?message=帳號、或密碼輸入錯誤")
 
 #驗證註冊網址
 @app.route("/signup",methods=["POST"])
 def signup():
-    # nameData=request.form["nameData"]
-    # usernameData=request.form["usernameData"]
-    # passwordData=request.form["passwordData"]
-    cur=mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
-    rv=cur.fetchall()
-    return str(rv)
-    # cursor.execute("SELECT * FROM member WHERE username =%s",usernameData)
-    # account=cursor.fetchone()
-    # if account:
-    #     return render_template("error.html")
+    nameData=request.form["nameData"]
+    usernameData=request.form["usernameData"]
+    passwordData=request.form["passwordData"]
+    mycursor=mydb.cursor()
+    mycursor.execute("SELECT * FROM member WHERE username =%s",(usernameData,))
+    user=mycursor.fetchone()
+    if usernameData=="" or passwordData == "" or nameData=="":
+        return redirect("/error?message=請輸入姓名、帳號、密碼")
+    elif user != None:
+        return redirect("/error?message=帳號已經被註冊")
+    else:
+        mycursor.execute("INSERT INTO member (name, username, password) VALUES (%s,%s,%s)",(nameData,usernameData,passwordData,))
+        mydb.commit()
+        session["name"]=request.form["nameData"]
+        session["username"]=request.form["usernameData"]
+        session["password"]=request.form["passwordData"]
+        return redirect("/")
 
 
 #登出成功網址
+@app.route("/signout")
+def signout():
+    del session["username"]
+    return redirect("/")
 
 
 
